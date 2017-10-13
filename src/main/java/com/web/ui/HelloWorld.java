ee8e54c08e7ffea4b6cc2fb0175211840693d54d
wangzheng <wangzheng@clustertech.com>
2017-10-13 13:45:51 +0800

    add file

---
+++ src/main/java/com/web/ui/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.web.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorld {
+
+    @RequestMapping("/helloworld")
+    public String hello() {
+        System.out.println("hello world");
+        return "success";
+    }
+}
